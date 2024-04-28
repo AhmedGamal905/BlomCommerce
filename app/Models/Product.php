8a081f055082b2d24af5ed91df8d8771abcac463
ahmedgamal905 <ahmed.gamal9@hotmail.com>
2024-04-28 04:13:11 +0300

    create and upload imgs

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Product extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+    protected $fillable = [
+        'title',
+        'price',
+        'category_id',
+        'image',
+        'description',
+    ];
+
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('product_images');
+    }
+}
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard.welcome');
+    return view('dashboard.products.create');
 })->name('welcome');
+
+Route::prefix('/dashboard')->name('admin.')->group(function () {
+    Route::resource('/products', ProductController::class)->except('show');
+});
